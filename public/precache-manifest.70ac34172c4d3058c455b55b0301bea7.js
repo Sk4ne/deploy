adfc4886a1e3154525f8342328f1f923d8339367
Nlopezp <oldschool##35>
2020-03-02 12:15:25 -0500

    Deploy gouniversity

---
+++ public/precache-manifest.70ac34172c4d3058c455b55b0301bea7.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62f1bd6df8b583518c5c",
+    "url": "/css/app.d5ec8367.css"
+  },
+  {
+    "revision": "ec53fc9cf29b06ab0367",
+    "url": "/css/chunk-vendors.7e802ee8.css"
+  },
+  {
+    "revision": "6a89bca38d117386150d10fb1409e050",
+    "url": "/img/art.6a89bca3.svg"
+  },
+  {
+    "revision": "9b488224b0900d7c6b1a1fb53aae89a9",
+    "url": "/img/basketball.9b488224.svg"
+  },
+  {
+    "revision": "87899d6a0d61c08fdc3e2790f4413f88",
+    "url": "/img/book.87899d6a.svg"
+  },
+  {
+    "revision": "81bef2bcd571f7968fcd17e67450457f",
+    "url": "/img/earth-globe.81bef2bc.svg"
+  },
+  {
+    "revision": "3ac83d491247ff662a285b5b67f34c5e",
+    "url": "/img/lunch.3ac83d49.svg"
+  },
+  {
+    "revision": "b148b276f5e86205a059a064c1378ec3",
+    "url": "/img/mouse.b148b276.svg"
+  },
+  {
+    "revision": "5b96c8dadeaa2348a7a06ce8a7d73a3d",
+    "url": "/img/tv.5b96c8da.svg"
+  },
+  {
+    "revision": "fd02097c195b964a875fd9c22aa69e7d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "62f1bd6df8b583518c5c",
+    "url": "/js/app.7c116b66.js"
+  },
+  {
+    "revision": "ec53fc9cf29b06ab0367",
+    "url": "/js/chunk-vendors.a195386e.js"
+  },
+  {
+    "revision": "b4e0188130a8ac2e3a9da8ec350357c3",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
